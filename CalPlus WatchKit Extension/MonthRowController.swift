681db1c58d36990946fa11a2f212818dbe58d03e
Jared Poetter <poet0007@umn.edu>
2017-11-18 20:25:08 -0600

    Adding a months row controller and also changing the size of the table rows.

---
+++ CalPlus WatchKit Extension/MonthRowController.swift
@@ -0,0 +1,23 @@
+//
+//  MonthRowController.swift
+//  CalPlus WatchKit Extension
+//
+//  Created by Jared Poetter on 11/18/17.
+//  Copyright © 2017 Jared Poetter. All rights reserved.
+//
+
+import WatchKit
+
+class MonthRowController: NSObject {
+
+    @IBOutlet var label: WKInterfaceLabel!
+
+    //    func setRowTypes(_ rowTypes: [String]) {
+    //
+    //    }
+
+    //    func rowController(at index: Int) -> Any? {
+    //        return 0
+    //    }
+
+}
